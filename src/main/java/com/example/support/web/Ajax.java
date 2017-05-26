3556f570918c6c954c47ab270c1454476093e79b
Marcin Stachniuk <mstachniuk@gmail.com>
2017-05-26 21:15:21 +0200

    init version based on https://github.com/kolorobot/spring-mvc-quickstart-archetype
    some stuff removed

---
+++ src/main/java/com/example/support/web/Ajax.java
@@ -0,0 +1,11 @@
+package com.example.support.web;
+
+public class Ajax {
+
+	private Ajax() {
+	}
+
+	public static boolean isAjaxRequest(String requestedWith) {
+		return requestedWith != null ? "XMLHttpRequest".equals(requestedWith) : false;
+	}
+}
